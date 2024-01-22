1eab9f1fc1bfff9b6ac1f5e9f4269462ce7a50ae
shiamsharif <shiam.sharif.07@gmail.com>
2024-01-22 13:15:56 +0600

    blogs/urls.py commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView, BlogCreateView #new
+from .views import (
+    BlogListView,
+    BlogDetailView, 
+    BlogCreateView,
+    BlogUpdateView,
+)
 
 urlpatterns = [
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'), #newww
     path('post/new', BlogCreateView.as_view(), name='post_new'), #neww
     path('post/<int:pk>/', BlogDetailView.as_view(), name = 'post_detail'), #new
     path('', BlogListView.as_view(), name='home'),
